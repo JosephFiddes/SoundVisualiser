4832c48812d4bb32c8ddd58d0eec2a79af7b60eb
JDFIDDES <jdfiddes@student.unimelb.edu.au>
2022-01-19 13:49:09 +1100

    schmoovin

--- main.cpp
+++ main.cpp
@@ -9,21 +9,25 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<chrono>
+#include<thread>
+//#include<Windows.h>
 
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
 
-/**
-* Takes the source code of two shaders, and turns them into
-* a shader program.
-*/
-GLuint shadersToProgram(const char** source1, GLenum type1, const char** source2, GLenum type2);
+// FUNCTION DECLARATIONS **********************
+int getTime();
 
+// MAIN *******************************************
 int main() {
 	const int WIDTH = 800;
 	const int HEIGHT = 800;
+	const int FRAME_PERIOD = 17; // ms
+
+	srand(time(NULL));
 
 	// Initialise GLFW
 	glfwInit();
@@ -48,12 +52,13 @@ int main() {
 	
 	// Triforce
 	GLfloat vertices[] = {
-		-0.5f,     -0.5f * float(sqrt(3)) / 3,       0.0f, // Lower left corner
-		0.5f,      -0.5f * float(sqrt(3)) / 3,       0.0f, // Lower right corner
-		0.0f,      0.5f * float(sqrt(3)) * 2.0f / 3, 0.0f, // Upper corner
-		-0.5f / 2, 0.5f * float(sqrt(3)) / 6,        0.0f, // Inner left
-		0.5f / 2,  0.5f * float(sqrt(3)) / 6,        0.0f, // Inner right
-		0.0f,      -0.5f * float(sqrt(3)) / 3,       0.0f, // Middle bottom
+		// CO-ORDS ----------------------------------- \\  // COLOURS -------- \\.
+		-0.5f,     -0.5f * float(sqrt(3)) / 3,       0.0f, 0.02f, 0.3f,  0.8f,  // Lower left corner
+		0.5f,      -0.5f * float(sqrt(3)) / 3,       0.0f, 0.8f,  0.3f,  0.02f, // Lower right corner
+		0.0f,      0.5f * float(sqrt(3)) * 2.0f / 3, 0.0f, 0.32f, 0.6f,  1.0f,  // Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6,        0.0f, 0.9f,  0.45f, 0.17f, // Inner left
+		0.5f / 2,  0.5f * float(sqrt(3)) / 6,        0.0f, 0.9f,  0.45f, 0.17f, // Inner right
+		0.0f,      -0.5f * float(sqrt(3)) / 3,       0.0f, 0.8f,  0.3f,  0.02f // Middle bottom
 	};
 
 	GLuint indices[] = {
@@ -71,54 +76,96 @@ int main() {
 	// Generates Shader object using shaders default.vert and default.frag.
 	Shader shaderProgram("default.vert", "default.frag");
 
-	// Generates vertex array object and binds it.
-	VAO VAO1;
-	VAO1.Bind();
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
-	// Generates vertex buffer object and links it to vertices.
-	VBO VBO1(vertices, sizeof(vertices));
-	// Generates element buffer object and links it to indices.
-	EBO EBO1(indices, sizeof(indices));
-
-	// Links VBO to VAO.
-	VAO1.LinkVBO(VBO1, 0);
-
-	// Unbind all to prevent accidentally modifying them.
-	VAO1.Unbind();
-	VBO1.Unbind();
-	EBO1.Unbind();
-
-	// Swap the front and back buffers.
-	// (NOTE: The front buffer is the current screen, 
-	// the back buffer is the next frame (in preparation for it to be drawn)).
-	glfwSwapBuffers(window);
+	int prev_frame = 0;
+	int now = 0;
+	int frameCounter = 0;
 
 	// Run continuously, until the window is closed.
 	while (!glfwWindowShouldClose(window)) {
+		// Move vertices and colours a little bit.
+		int i = 0; 
+		for (GLfloat num : vertices) {
+			num += (GLfloat)(rand() / (float)RAND_MAX / 100.0f - 0.005f);
+
+			if (num < -1.0f) {
+				num = -1.0f;
+			}
+			else if (num > 1.0f) {
+				num = 1.0f;
+			}
+
+			vertices[i] = num;
+			i++;
+		}
+		
+		/*
+		for (GLfloat num : vertices) {
+			std::cout << num << " ";
+		}
+		std::cout << std::endl;
+		*/
+		
 		// Specify background colour.
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 
 		// Clear the back buffer and assign the new colour to it.
 		glClear(GL_COLOR_BUFFER_BIT);
-		 
-		shaderProgram.Activate();
+
+		// Generates vertex array object and binds it.
+		VAO VAO1;
 		VAO1.Bind();
+
+		// Generates vertex buffer object and links it to vertices.
+		VBO VBO1(vertices, sizeof(vertices));
+		// Generates element buffer object and links it to indices.
+		EBO EBO1(indices, sizeof(indices));
+		// Links VBO to VAO.
+		VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+		VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+		// Unbind all to prevent accidentally modifying them.
+		VAO1.Unbind();
+		VBO1.Unbind();
+		EBO1.Unbind();
+
+		GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+		shaderProgram.Activate();
+		glUniform1f(uniID, 1.0f);
+		VAO1.Bind(); 
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+
+		// Wait until next frame.
+		std::this_thread::sleep_for(std::chrono::milliseconds(
+			prev_frame + FRAME_PERIOD - getTime() - 10));
+		prev_frame = getTime();
 
 		glfwSwapBuffers(window);
 
 		// Poll for events that may or may not happen (who knows.......)
 		glfwPollEvents();
+
+		// Delete stuff.
+		VAO1.Delete();
+		VBO1.Delete();
+		EBO1.Delete();
+
+		frameCounter++;
 	}
 
-	// Delete stuff.
-	VAO1.Delete();
-	VBO1.Delete();
-	EBO1.Delete();
+	
 	shaderProgram.Delete();
 
 	// End by closing a bunch of stuff.
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
+}
+
+int getTime() {
+	using namespace std::chrono;
+	return duration_cast<milliseconds>(system_clock::now()
+		.time_since_epoch()).count();
 }
--- shaderClass.cpp
+++ shaderClass.cpp
@@ -27,17 +27,20 @@ Shader::Shader(const char* vertexFile, const char* fragmentFile) {
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
 	glCompileShader(vertexShader);
+	compileErrors(vertexShader, "VERTEX");
 
 	// Creates and compiles a fragment shader based on fragmentShaderSource
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-	glCompileShader(fragmentShader);
+	glCompileShader(fragmentShader); 
+	compileErrors(vertexShader, "FRAGMENT");
 
 	// Combines the two shaders into one thing.
 	ID = glCreateProgram();
 	glAttachShader(ID, vertexShader);
 	glAttachShader(ID, fragmentShader);
 	glLinkProgram(ID);
+	compileErrors(ID, "PROGRAM");
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 }
@@ -48,4 +51,23 @@ void Shader::Activate() {
 
 void Shader::Delete() {
 	glDeleteProgram(ID);
+}
+
+void Shader::compileErrors(unsigned int shader, const char* type) {
+	GLint hasCompiled;
+	char infoLog[1024];
+	if (type != "PROGRAM") {
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &hasCompiled);
+		if (hasCompiled == GL_FALSE) {
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "SHADER_COMPILATION_ERROR for:" << type << "\n" << std::endl;
+		}
+	}
+	else {
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &hasCompiled);
+		if (hasCompiled == GL_FALSE) {
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "SHADER_LINKING_ERROR for:" << type << "\n" << std::endl;
+		}
+	}
 }
